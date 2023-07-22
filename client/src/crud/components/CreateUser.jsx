c2832c0ce2d541743cb790d7293abe51770ad210
nadeeauliahq <129242513+nadeeauliahq@users.noreply.github.com>
2023-07-23 00:51:58 +0700

    Update CreateUser.jsx

--- client/src/crud/components/CreateUser.jsx
+++ client/src/crud/components/CreateUser.jsx
@@ -38,7 +38,7 @@ function CreateUser() {
 
     // Submit form if all fields are filled
     axios
-      .post("https://deploy-project-new.vercel.app/create", { name, forWho, message })
+      .post("https://cobalagi-api.vercel.app/create", { name, forWho, message })
       .then((result) => {
         console.log(result);
         navigate("/users");
