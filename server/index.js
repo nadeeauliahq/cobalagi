ee2b7951cc38e9a332389831083a7fa9a439f1f0
nadeeauliahq <129242513+nadeeauliahq@users.noreply.github.com>
2023-07-23 00:59:59 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://cobalagi-fe.vercel.app"],
+    origin: ["*"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
   }
